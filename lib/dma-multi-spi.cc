126cf93791a687c9b5716d4a221db34e1f6237ef
Henner Zeller <h.zeller@acm.org>
2016-05-29 11:06:40 -0700

    o Switch to LGPL.

--- lib/dma-multi-spi.cc
+++ lib/dma-multi-spi.cc
@@ -2,18 +2,18 @@
 // SPI Pixels - Control SPI LED strips (spixels)
 // Copyright (C) 2016 Henner Zeller <h.zeller@acm.org>
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "multi-spi.h"
 
--- lib/rpi-dma.h
+++ lib/rpi-dma.h
@@ -1,17 +1,19 @@
 // -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// SPI Pixels - Control SPI LED strips (spixels)
 // Copyright (C) 2016 Henner Zeller <h.zeller@acm.org>
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation version 2.
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://gnu.org/licenses/gpl-2.0.txt>
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifdef  __cplusplus
 extern "C" {
