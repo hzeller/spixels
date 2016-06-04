d852a75abfb568265dd63c5bc9bc028a8e47e882
Henner Zeller <h.zeller@acm.org>
2016-06-03 22:51:12 -0700

    o Provide a raw linear LEDStrip::SetLinearValue() method with   16-bit resolution of RGB values - so the implementation has the   freedom to provide whatever it can do (5 Bit LPD6803, up to 12Bit APA102)   This is the only strip-hardware dependent implementation. o Make SetPixel() that does luminance correction and SetBrightness()   a common implementation in the base class.

--- include/led-strip.h
+++ include/led-strip.h
@@ -28,12 +28,12 @@ namespace spixels {
 
 // Red Green Blue color. A color represented by RGB values.
 struct RGBc {
-    RGBc(): r(0), g(0), b(0){}
+    RGBc(): r(0), g(0), b(0) {}
 
     // Creating a color with the Red/Green/Blue components. If you are compiling
     // with C++11, you can even do that in-line
     // LEDStrip::SetPixel(0, {255, 255, 255});
-    RGBc(uint8_t red, uint8_t green, uint8_t blue) : r(red), g(green), b(blue){}
+    RGBc(uint8_t red, uint8_t green, uint8_t blue) : r(red), g(green), b(blue) {}
 
     // Setting a color from a single 24Bit 0xRRGGBB hex-value, e.g. 0xff00ff
     RGBc(uint32_t hexcolor)
@@ -49,12 +49,17 @@ struct RGBc {
 // Simplest possible way for a LED strip.
 class LEDStrip {
 public:
-    virtual ~LEDStrip() {}
+    virtual ~LEDStrip();
+
+    // Return number of attached LEDs.
+    inline int count() const { return count_; }
 
     // Set pixel color. Input it RGB, output is luminance corrected
     // you don't have to apply pre-correction.
-    virtual void SetPixel(int pos, const RGBc& c) = 0;
+    // This is typically the function to use.
+    void SetPixel(int pos, const RGBc& c);
 
+    // Same with explicitly spelled out r,g,b.
     void SetPixel(int pos, uint8_t r, uint8_t g, uint8_t b) {
         SetPixel(pos, RGBc(r, g, b));
     }
@@ -62,18 +67,32 @@ public:
     // Set overall brightness for all pixels. Range of [0 .. 255].
     // This scales the brightness so that it looks linear luminance corrected
     // for the eye.
+    // This will be only having a somewhat pleasing result for LED strips with
+    // higher PWM resolution (such as APA102).
     //
-    // Not all LEDStrips have this implemented as many have not enough
-    // PWM resolution for this to be useful. Only APA102 provides this now.
-    virtual void SetBrightness(uint8_t brigthness) {}
+    // Brightness change will take effect with next SendBuffers().
+    void SetBrightness(uint8_t brigthness);
+    inline uint8_t brightness() const { return brightness_; }
 
-    // Return number of attached LEDs.
-    inline int count() const { return count_; }
-
+    // Set the raw, linear RGB value as provided by the LED strip, normalized
+    // to the range [0 .. 0xFFFF]. This is LED-Strip dependent.
+    //
+    // The range is always [0 .. 0xFFFF], but the implementation internally
+    // scales it to whatever the hardware can do (LPD6803 only uses the
+    // 5 Most significant bits, while APA102 can provide up to 12 bit
+    // resolution depending on the circumstances).
+    //
+    // Note, this is the _linear_ range provided by the RGB strip as
+    // opposed to the luminance corrected RGB value in SetPixel(). So only
+    // use if you need the direct values.
+    virtual void SetLinearValues(int pos,
+                                 uint16_t r, uint16_t g, uint16_t b) = 0;
 protected:
-    LEDStrip(int count) : count_(count) {}
+    LEDStrip(int count);
 
     const int count_;
+    RGBc *const values_;
+    uint8_t brightness_;
 };
 
 // Factories for various LED strips.
--- lib/led-strip.cc
+++ lib/led-strip.cc
@@ -22,12 +22,11 @@
 #include "multi-spi.h"
 #include "led-strip.h"
 
-static const int kBitPlanes = 16;
 typedef uint16_t CIEValue;
 
 // Do CIE1931 luminance correction and scale to maximum expected output bits.
 static CIEValue luminance_cie1931_internal(uint8_t c, uint8_t brightness) {
-    const float out_factor = ((1 << kBitPlanes) - 1);
+    const float out_factor = 0xFFFF;
     const float v = 100.0f * (brightness/255.0f) * (c/255.0f);
     return out_factor * ((v <= 8) ? v / 902.3 : pow((v + 16) / 116.0, 3));
 }
@@ -44,12 +43,35 @@ static CIEValue *CreateCIE1931LookupTable() {
 
 // Return a CIE1931 corrected value from given desired lumninace value and
 // brightness.
-static CIEValue luminance_cie1931(uint8_t bits, uint8_t value, uint8_t bright) {
+static CIEValue luminance_cie1931(uint8_t value, uint8_t bright) {
     static const CIEValue *const luminance_lookup = CreateCIE1931LookupTable();
-    return luminance_lookup[bright * 256 + value] >> (kBitPlanes - bits);
+    return luminance_lookup[bright * 256 + value];
 }
 
 namespace spixels {
+LEDStrip::LEDStrip(int count)
+    : count_(count), values_(new RGBc[count]), brightness_(255) {
+}
+
+LEDStrip::~LEDStrip() { delete values_; }
+
+void LEDStrip::SetPixel(int pos, const RGBc& c) {
+    if (pos < 0 || pos >= count()) return;
+    values_[pos] = c;
+    SetLinearValues(pos,
+                    luminance_cie1931(c.r, brightness_),
+                    luminance_cie1931(c.g, brightness_),
+                    luminance_cie1931(c.b, brightness_));
+}
+
+void LEDStrip::SetBrightness(uint8_t new_brightness) {
+    if (new_brightness == brightness_) return;
+    brightness_ = new_brightness;
+    for (int i = 0; i < count_; ++i) {
+        SetPixel(i, values_[i]);  // Force recalculation.
+    }
+}
+
 namespace {
 class WS2801LedStrip : public LEDStrip {
 public:
@@ -57,12 +79,11 @@ public:
         : LEDStrip(count), spi_(spi), gpio_(gpio) {
         spi_->RegisterDataGPIO(gpio, count * 3);
     }
-    virtual void SetPixel(int pos, const RGBc &c) {
-        if (pos < 0 || pos >= count_) return;
-        const uint8_t br = 255;  // brightness.
-        spi_->SetBufferedByte(gpio_, 3 * pos + 0, luminance_cie1931(8, c.r, br));
-        spi_->SetBufferedByte(gpio_, 3 * pos + 1, luminance_cie1931(8, c.g, br));
-        spi_->SetBufferedByte(gpio_, 3 * pos + 2, luminance_cie1931(8, c.b, br));
+
+    virtual void SetLinearValues(int pos, uint16_t r, uint16_t g, uint16_t b) {
+        spi_->SetBufferedByte(gpio_, 3 * pos + 0, r >> 8);
+        spi_->SetBufferedByte(gpio_, 3 * pos + 1, g >> 8);
+        spi_->SetBufferedByte(gpio_, 3 * pos + 2, b >> 8);
     }
 
 private:
@@ -76,22 +97,23 @@ public:
         : LEDStrip(count), spi_(spi), gpio_(gpio) {
         const size_t bytes_needed = 4 + 2 * count + 4;
         spi_->RegisterDataGPIO(gpio, bytes_needed);
+
         // Four zero bytes as start-bytes for lpd6803
         spi_->SetBufferedByte(gpio_, 0, 0x00);
         spi_->SetBufferedByte(gpio_, 1, 0x00);
         spi_->SetBufferedByte(gpio_, 2, 0x00);
         spi_->SetBufferedByte(gpio_, 3, 0x00);
-        for (int pos = 0; pos < count; ++pos)
+        for (int pos = 0; pos < count; ++pos) {
             SetPixel(pos, 0x000000);     // Initialize all top-bits.
+        }
     }
 
-    virtual void SetPixel(int pos, const RGBc &c) {
-        if (pos < 0 || pos >= count_) return;
+    virtual void SetLinearValues(int pos, uint16_t r, uint16_t g, uint16_t b) {
         uint16_t data = 0;
         data |= (1<<15);  // start bit
-        data |= luminance_cie1931(5, c.r, 255) << 10;
-        data |= luminance_cie1931(5, c.g, 255) <<  5;
-        data |= luminance_cie1931(5, c.b, 255) <<  0;
+        data |= (r >> 11) << 10;
+        data |= (g >> 11) <<  5;
+        data |= (b >> 11) <<  0;
 
         spi_->SetBufferedByte(gpio_, 2 * pos + 4 + 0, data >> 8);
         spi_->SetBufferedByte(gpio_, 2 * pos + 4 + 1, data & 0xFF);
@@ -105,8 +127,7 @@ private:
 class APA102LedStrip : public LEDStrip {
 public:
     APA102LedStrip(MultiSPI *spi, int gpio, int count)
-        : LEDStrip(count), spi_(spi), gpio_(gpio), global_brightness_(255),
-          values_(new RGBc[count]) {
+        : LEDStrip(count), spi_(spi), gpio_(gpio) {
         const size_t startframe_size = 4;
         const size_t endframe_size = (count+15) / 16;
         const size_t bytes_needed = startframe_size + 4*count + endframe_size;
@@ -130,54 +151,37 @@ public:
         }
     }
 
-    virtual ~APA102LedStrip() { delete values_; }
-
-    virtual void SetPixel(int pos, const RGBc &c) {
-        if (pos < 0 || pos >= count_) return;
-        values_[pos] = c;
+    virtual void SetLinearValues(int pos, uint16_t r, uint16_t g, uint16_t b) {
         const int base = 4 + 4 * pos;
-        uint16_t rr = luminance_cie1931(12, c.r, global_brightness_);
-        uint16_t gg = luminance_cie1931(12, c.g, global_brightness_);
-        uint16_t bb = luminance_cie1931(12, c.b, global_brightness_);
+        r >>= 4; g >>= 4; b >>= 4;
 
         // If value is dim, use the APA global brightness adjustment for
         // more resolution. We essentially get 4 bits at the bottom end.
-        const uint16_t bit_use = rr | gg | bb;  // find highest bit used.
+        const uint16_t bit_use = r | g | b;  // find highest bit used.
         if (bit_use < 16) {
             spi_->SetBufferedByte(gpio_, base + 0, 0xE0 | 0x01);
         } else if (bit_use < 32) {
             spi_->SetBufferedByte(gpio_, base + 0, 0xE0 | 0x03);
-            rr >>= 1; gg >>= 1; bb >>= 1;
+            r >>= 1; g >>= 1; b >>= 1;
         } else if (bit_use < 64) {
             spi_->SetBufferedByte(gpio_, base + 0, 0xE0 | 0x07);
-            rr >>= 2; gg >>= 2; bb >>= 2;
+            r >>= 2; g >>= 2; b >>= 2;
         } else if (bit_use < 128) {
             spi_->SetBufferedByte(gpio_, base + 0, 0xE0 | 0x0F);
-            rr >>= 3; gg >>= 3; bb >>= 3;
+            r >>= 3; g >>= 3; b >>= 3;
         } else {
             spi_->SetBufferedByte(gpio_, base + 0, 0xE0 | 0x1F);
-            rr >>= 4; gg >>= 4; bb >>= 4;
+            r >>= 4; g >>= 4; b >>= 4;
         }
 
-        spi_->SetBufferedByte(gpio_, base + 1, bb);
-        spi_->SetBufferedByte(gpio_, base + 2, gg);
-        spi_->SetBufferedByte(gpio_, base + 3, rr);
-    }
-
-    virtual void SetBrightness(uint8_t new_brightness) {
-        if (new_brightness != global_brightness_) {
-            global_brightness_ = new_brightness;
-            for (int i = 0; i < count_; ++i) {
-                SetPixel(i, values_[i]);  // Force recalculation.
-            }
-        }
+        spi_->SetBufferedByte(gpio_, base + 1, b);
+        spi_->SetBufferedByte(gpio_, base + 2, g);
+        spi_->SetBufferedByte(gpio_, base + 3, r);
     }
 
 private:
     MultiSPI *const spi_;
     const int gpio_;
-    uint8_t global_brightness_;
-    RGBc *values_;
 };
 }  // anonymous namespace
 
