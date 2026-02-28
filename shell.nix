a3b1717ea8d28ea6842ddd609d8fe7a365ffbf44
Henner Zeller <h.zeller@acm.org>
2026-02-28 10:59:35 +0100

    shell.nix: get newer kicad + compile fix to gerbv

--- shell.nix
+++ shell.nix
@@ -2,17 +2,18 @@
 # If you have nix installed, you may simply run `nix-shell`
 # in this repo, and have all dependencies ready in the new shell.
 
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz") {} }:
+# nixpkgs with current kicad 9.0.7 and a compile fix to gerbv
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b46e7e3879793c3dd810f9c315d9dc211f550af3.tar.gz") {} }:
 pkgs.mkShell {
   buildInputs = with pkgs;
     [
+      kicad
       git
       python3
       python3Packages.kicad
       # Kicad packages refer to this, but don't
       # bring into dependencies in 25.11
       python3Packages.wxpython
-      kicad
       zip
       gerbv
     ];
